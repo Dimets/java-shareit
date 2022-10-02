307b474c9e15a162235878b4bf215bbc3d3672d6
Dimets <dimets1977@gmail.com>
2022-10-02 14:53:56 +0300

    test: увеличить покрытие тестами

--- src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -8,7 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import ru.practicum.shareit.exception.CommentValidationException;
 import ru.practicum.shareit.exception.EmailFormatException;
+import ru.practicum.shareit.exception.EntityNotFoundException;
+import ru.practicum.shareit.exception.UsersDoNotMatchException;
 import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
 
@@ -40,7 +43,25 @@ public class ItemServiceImplTest {
         final CommentValidationException exception = Assertions.assertThrows(
                 CommentValidationException.class,
                 () -> itemService.create(1L, 1L, commentDto));
-
-
     }
+
+    @Test
+    @Sql({"/schema.sql"})
+    @Sql(scripts = "/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void updateWithUsersDoNotMatchException() throws EmailFormatException, EntityNotFoundException {
+        userDto = userService.create(new UserDto(1L, "name", "name@email"));
+
+        UserDto wrongUserDto = userService.create(new UserDto(5L, "name", "wrongname@email"));
+
+
+        ItemDto itemDto = new ItemDto(1L, "first item  name", "first item description",
+                Boolean.TRUE, 1L, null);
+
+        itemService.create(userDto.getId(), itemDto);
+
+        final UsersDoNotMatchException exception = Assertions.assertThrows(
+                UsersDoNotMatchException.class,
+                () -> itemService.update(wrongUserDto.getId(), itemDto));
+    }
+
 }
