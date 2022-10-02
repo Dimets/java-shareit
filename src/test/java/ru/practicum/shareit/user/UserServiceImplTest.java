8c39f2943ab1b6080ceb1373a41175463043516b
Dimets <dimets1977@gmail.com>
2022-10-02 15:13:14 +0300

    test: увеличить покрытие тестами

--- src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -10,6 +10,8 @@ import ru.practicum.shareit.exception.EmailFormatException;
 import ru.practicum.shareit.exception.EntityNotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class UserServiceImplTest {
@@ -40,4 +42,43 @@ public class UserServiceImplTest {
                 () -> userService.findById(-99L));
     }
 
+    @Test
+    @Sql({"/schema.sql"})
+    @Sql(scripts = "/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void create() throws Exception {
+        userDto = userService.create(new UserDto(1L, "name", "user@email"));
+
+        assertThat(userDto.getId()).isEqualTo(1L);
+        assertThat(userDto.getEmail()).isEqualTo("user@email");
+        assertThat(userDto.getName()).isEqualTo("name");
+
+    }
+
+    @Test
+    @Sql({"/schema.sql"})
+    @Sql(scripts = "/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void deleteById() throws EmailFormatException {
+        userDto = userService.create(new UserDto(1L, "name", "user@email"));
+
+        userService.deleteById(userDto.getId());
+
+        final EntityNotFoundException exception = Assertions.assertThrows(
+                EntityNotFoundException.class,
+                () -> userService.findById(userDto.getId()));
+
+    }
+
+    @Test
+    @Sql({"/schema.sql"})
+    @Sql(scripts = "/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void update() throws Exception {
+        userDto = userService.create(new UserDto(1L, "name", "user@email"));
+
+        userDto.setName("updated name");
+
+        UserDto result = userService.update(userDto);
+
+        assertThat(result.getName()).isEqualTo("updated name");
+        assertThat(result.getEmail()).isEqualTo(userDto.getEmail());
+    }
 }
